655b8f953975b9c1e0876ab50cce7f9c965f5e67
caducoder <cadudrop@hotmail.com>
2024-01-13 17:48:41 -0300

    feat: get the selected answers from user

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quiz_questions from '../../../assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+
+  title: string = ''
+
+  questions: any
+  questionSelected: any
+
+  answers: string[] = []
+  answerSelected: string = ''
+
+  index: number = 0
+  maxIndex: number = 0
+
+  finished: boolean = false
+
+  ngOnInit(): void {
+    if (quiz_questions) {
+      this.finished = false
+      this.title = quiz_questions.title
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.index]
+
+      this.index = 0
+      this.maxIndex = this.questions.length
+    }
+  }
+
+  selectOption(value: string) {
+    this.answers.push(value)
+    this.index += 1
+
+    if (this.maxIndex > this.index) {
+      this.questionSelected = this.questions[this.index]
+    } else {
+      this.finished = true
+    }
+  }
 
 }
